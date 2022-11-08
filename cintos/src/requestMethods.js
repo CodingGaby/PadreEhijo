c0d6af78606ea22b2113109823f67adc073d08a1
Bloss <macma.cube1@gmail.com>
2022-11-07 21:35:23 -0600

    3 api

--- cintos/src/requestMethods.js
+++ cintos/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://cafcintos-production.up.railway.app/api/";
+const BASE_URL = "http://localhost:5000/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNThkMjYyNTg4YjkyZjFjOGZjZTY3YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2Njc2NTUyMiwiZXhwIjoxNjY3MDI0NzIyfQ.uj1WgDJjtMuNqZRF5yBARmF3iuDCJ6aKNJnHlaXl1Ik";
 
 export const publicRequest = axios.create({
