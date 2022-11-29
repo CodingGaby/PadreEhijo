fb79aaad790cb0fea6782e06c5b8de02650454c8
Bloss <macma.cube1@gmail.com>
2022-11-29 12:38:42 -0600

    RECONF api

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-// const stripe = require("stripe")(process.env.STRIPE_KEY);
-const KEY = process.env.STRIPE_KEY
-const stripe = require("stripe")(KEY);
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
